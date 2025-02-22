6e1ba92df3f068acbb05d821278055b22ae1af4a
michaelcogossi <michael.cogossi254@gmail.com>
2025-02-22 11:37:08 -0500

    Catalog Feature

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,46 @@
+export default {
+    products: [
+      {
+        name: "Shirt",
+        imageUrl: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.5,
+        numberOfReviews: 10,
+      },
+      {
+        name: "Shirt",
+        imageUrl: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.5,
+        numberOfReviews: 10,
+      },
+      {
+        name: "Pants",
+        imageUrl: "/images/d2.jpg",
+        price: 80,
+        brand: "Adidas",
+        rating: 4.0,
+        numberOfReviews: 15,
+      },
+      {
+        name: "Shoes",
+        imageUrl: "/images/d3.jpg",
+        price: 120,
+        brand: "Puma",
+        rating: 4.8,
+        numberOfReviews: 22,
+      },
+      {
+        name: "Hat",
+        imageUrl: "/images/d4.jpg",
+        price: 25,
+        brand: "Under Armour",
+        rating: 4.2,
+        numberOfReviews: 5,
+      },
+    ],
+  };
+  
+  
