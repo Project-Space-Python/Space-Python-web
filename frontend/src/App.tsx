d853acec7e81b1c9d6bde5398aa49a32e69d31b1
Ashton Forsberg <ashtonforsbergg@gmail.com>
2025-02-22 11:12:29 -0500

    Adding Home and Products to SRC

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/Home";
+import Products from "./Products/Products";
 
 function App() {
   const openMenu = () => {
@@ -10,11 +13,10 @@ function App() {
     document.querySelector(".sidebar")?.classList.remove("open");
   }
   return (
+    <Router>
     <div className="grid-container">
-    <head>
     <link rel="stylesheet" href= "style.css"></link>
         <title>Space Python</title>
-            </head>
     <body>
             <header className = "header">
                 <div className = "brand">
@@ -22,8 +24,8 @@ function App() {
                     <a href = "index.html">Space Python</a>
                 </div>
                 <div className="header-links">
-                    <a href = "cart.html">Cart</a>
-                    <a href = "signin.html">Sign In</a>
+                    <Link to="/">Home</Link>
+                    <Link to="/catalog">Catalog</Link>
                 </div>
             </header> 
             <aside className="sidebar">
@@ -35,63 +37,18 @@ function App() {
                 </ul>
             </aside>
             <main className = "main">
-                <ul>
-                    <div className="content">
-                        <ul className="products">
-                            <li>
-                                <div className="product">
-                                    <img className="product-image" src="images/d1.jpg" alt="product" />
-                                    <div className="product-name">
-                                        <a href="product.html">Slim Shirt</a>
-                                    </div>
-                                    <div className="product-brand">Nike</div>
-                                    <div className="product-price">$60</div>
-                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="product">
-                                    <img className="product-image" src="images/d1.jpg" alt="product" />
-                                    <div className="product-name">
-                                        <a href="product.html">Slim Shirt</a>
-                                    </div>
-                                    <div className="product-brand">Nike</div>
-                                    <div className="product-price">$60</div>
-                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="product">
-                                    <img className="product-image" src="images/d1.jpg" alt="product" />
-                                    <div className="product-name">
-                                        <a href="product.html">Slim Shirt</a>
-                                    </div>
-                                    <div className="product-brand">Nike</div>
-                                    <div className="product-price">$60</div>
-                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="product">
-                                    <img className="product-image" src="images/d1.jpg" alt="product" />
-                                    <div className="product-name">
-                                        <a href="product.html">Slim Shirt</a>
-                                    </div>
-                                    <div className="product-brand">Nike</div>
-                                    <div className="product-price">$60</div>
-                                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>                
-                </ul>
+                <Routes>
+                    <Route path="/" element = {<Home />} />
+                    <Route path="/catalog" element={<Products />} />
+                </Routes>
             </main>
             <footer className = "footer">
                 &copy;2022 Space Python
             </footer>
             </body>
         </div>
-  );
+        </Router>
+  ); 
 }
 
 export default App;
