0fda915b661e9d64891c8a41c051fb0925d3d9b7
Ashton Forsberg <ashtonforsbergg@gmail.com>
2025-03-24 16:19:34 -0400

    pull

--- SpacePython.Api/obj/Debug/net9.0/SpacePython.Api.AssemblyInfo.cs
+++ SpacePython.Api/obj/Debug/net9.0/SpacePython.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpacePython.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02e82125a9fe150ca63b1cc30c627c5e15497543")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpacePython.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpacePython.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
