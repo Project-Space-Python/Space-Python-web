372f9e7946d20a1026deb46879ab7adbd7d9526f
michaelcogossi <michael.cogossi254@gmail.com>
2025-04-06 13:10:59 -0400

    Add unit tests for Home link and Home header using getByRole

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -7,3 +7,25 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+it('should render the Home link', () => {
+  render(<App />);
+  expect(screen.getByText('Home')).toBeInTheDocument();
+});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+it('should render the Home link', () => {
+  render(<App />);
+  // expect(screen.getAllByText('Home')).toBeInTheDocument();
+  expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+});
+
+it('should render the Home header', () => {
+  render(<App />);
+  expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
+});
